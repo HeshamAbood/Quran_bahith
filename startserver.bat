fe7aaf8d204e99d20e342b244844c508564c3e16
Hesham Abbod <hshm_aboud@yahoo.com>
2020-11-06 18:43:43 +0300

    Delete startserver.bat

--- startserver.bat
+++
@@ -1,3 +0,0 @@
-start C:\"Program Files (x86)"\Google\Chrome\Application\chrome.exe "http://127.0.0.1:8000/"
-cmd /k "activate C:\Users\habood\AppData\Local\Continuum\anaconda3\envs\py32 & e: & cd e:\projects\bahith\bahith2\ &  python manage.py runserver"
-
